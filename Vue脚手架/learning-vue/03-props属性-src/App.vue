ef53b9a4fbfdb6331d497dc4e1507dc707b84ce1
teng <teng2002823@outlook.com>
2022-09-11 17:34:51 +0800

    props属性

---
+++ Vue脚手架/learning-vue/03-props属性-src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Student name="李四" sex="女" :age="18"/>
+  </div>
+</template>
+
+<script>
+import Student from "@/components/Student";
+
+export default {
+  name: 'App',
+  components: {
+    Student,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue脚手架/learning-vue/03-props属性-src/components/Student.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="demo">
+    <h1>{{ msg }}</h1>
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生性别：{{ sex }}</h2>
+    <h2>学生年龄：{{ myAge + 1 }}</h2>
+    <button @click="updateAge">尝试修改收到的年龄</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    console.log(this);
+    return {
+      msg: "欢迎进行学习！",
+      myAge: this.age,  // props的优先级更高
+    }
+  },
+  methods: {
+    updateAge() {
+      this.myAge++; // 进行修改
+    }
+  },
+  // 简单声明接受
+  props: ['name', 'sex', 'age'],
+  // 接受的同时对数据进行类型限制
+  /*props: {
+    name: String,
+    sex: String,
+    age: Number,
+  }*/
+  // 接受的同时对数据进行类型限制+默认值的指定+必要性的限制
+  /*props: {
+    name: {
+      type: String, // nane类型
+      required: true, // 名字是必须的
+    },
+    age: {
+      type: Number,
+      default: 99,  // 默认值
+    },
+    sex: {
+      type: String,
+      required: true,
+    }
+  }*/
+}
+</script>
+
+<style scoped>
+#demo {
+  background: skyblue;
+}
+</style>
