185ad99d725e9718674be06bea6fc169d8d7c5a2
teng <teng2002823@outlook.com>
2022-09-11 17:35:49 +0800

    TodoList案例

---
+++ Vue脚手架/learning-vue/07-TodoList案例-src/components/List.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="todo-main">
+    <Item v-for="todoObj in todos"
+          :key="todoObj.id"
+          :todo="todoObj"
+          :checkItemTodo="checkTodo"
+          :deleteItemTodo="deleteTodo"/>
+  </ul>
+</template>
+
+<script>
+import Item from "@/components/Item";
+
+export default {
+  name: "List",
+  components: {Item},
+  props: ['todos', 'checkTodo', 'deleteTodo'],
+}
+</script>
+
+<style scoped>
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
