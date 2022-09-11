56d722175410778907989fe3ff8ebbefa363dae8
teng <teng2002823@outlook.com>
2022-09-11 17:35:30 +0800

    scoped样式

---
+++ Vue脚手架/learning-vue/06-scoped-src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="title">你好啊</h1>
+    <Student/>
+    <School/>
+  </div>
+</template>
+
+<script>
+import Student from "@/components/Student";
+import School from "@/components/School";
+
+export default {
+  name: 'App',
+  components: {
+    Student,
+    School
+  },
+}
+</script>
+
+<style scoped>
+.title {
+  color: red;
+}
+</style>
---
+++ Vue脚手架/learning-vue/06-scoped-src/components/Student.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="demo">
+    <h2 class="title">学生姓名：{{ name }}</h2>
+    <h2 class="teng">学生性别：{{ sex }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "张三",
+      sex: "男",
+    }
+  },
+}
+</script>
+
+<style scoped lang="less">
+.demo {
+  background-color: pink;
+
+  .teng {
+    font-size: 40px;
+  }
+}
+</style>
