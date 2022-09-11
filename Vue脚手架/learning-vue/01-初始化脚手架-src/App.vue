2ab5f7ea381519512fd7d91d526eec1a3aa7d33c
teng <teng2002823@outlook.com>
2022-09-11 17:34:32 +0800

    初始化脚手架

---
+++ Vue脚手架/learning-vue/01-初始化脚手架-src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import School from "@/components/Student";
+import Student from "@/components/Student";
+
+export default {
+  name: 'App',
+  components: {
+    School,
+    Student,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
