7dec2f959a218e063ab0d47ea9d4d0ae4e607f9f
teng <teng2002823@outlook.com>
2022-09-13 00:43:48 +0800

    用过渡与动画优化TodoList

---
+++ Vue脚手架/learning-vue/16-动画优化TodoList-src/components/List.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul class="todo-main">
+    <transition-group name="todo" appear>
+      <Item v-for="todoObj in todos"
+            :key="todoObj.id"
+            :todo="todoObj"/>
+    </transition-group>
+  </ul>
+</template>
+
+<script>
+import Item from "@/components/Item";
+
+export default {
+  name: "List",
+  components: {Item},
+  props: ['todos'],
+}
+</script>
+
+<style scoped>
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+
+.todo-enter-active {
+  animation: teng 0.5s linear;
+}
+
+
+.todo-leave-active {
+  animation: teng 0.5s linear reverse;
+}
+
+@keyframes teng {
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0px);
+  }
+}
+</style>
