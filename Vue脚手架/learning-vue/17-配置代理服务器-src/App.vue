0de5e057651b849b93f7310d3740d95008fb4735
teng <teng2002823@outlook.com>
2022-09-13 22:37:21 +0800

    配置代理服务器---解决跨域

---
+++ Vue脚手架/learning-vue/17-配置代理服务器-src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="getStudents">获取学生信息</button>
+    <button @click="getCars">获取汽车信息</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  methods: {
+    getStudents() {
+      // 记得加上前缀
+      axios.get("http://localhost:8080/api1/students").then(
+          response => {
+            console.log("请求成功了", response.data);
+          },
+          error => {
+            console.log("请求失败", error.message);
+          }
+      )
+    },
+    getCars() {
+      axios.get("http://localhost:8080/api2/cars").then(
+          response => {
+            console.log("请求成功了", response.data);
+          },
+          error => {
+            console.log("请求失败", error.message);
+          }
+      )
+    }
+  }
+}
+</script>
