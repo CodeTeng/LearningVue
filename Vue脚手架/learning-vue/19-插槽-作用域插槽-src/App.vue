d37bc52b7d1e07a193991bc853c869cddf7985b0
teng <teng2002823@outlook.com>
2022-09-13 22:37:49 +0800

    作用域插槽

---
+++ Vue脚手架/learning-vue/19-插槽-作用域插槽-src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <Category title="游戏">
+      <template scope="teng">
+        <ul>
+          <li v-for="(item,index) in teng.games" :key="index">{{ item }}</li>
+        </ul>
+      </template>
+    </Category>
+    <Category title="游戏">
+      <template scope="{games}">
+        <ol>
+          <li style="color: red" v-for="(item,index) in games" :key="index">{{ item }}</li>
+        </ol>
+      </template>
+    </Category>
+    <Category title="游戏">
+      <template slot-scope="{games}">
+        <h4 v-for="(item,index) in games" :key="index">{{ item }}</h4>
+      </template>
+    </Category>
+  </div>
+</template>
+
+<script>
+import Category from "@/components/Category";
+
+export default {
+  name: 'App',
+  components: {Category},
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
+
---
+++ Vue脚手架/learning-vue/19-插槽-作用域插槽-src/components/Category.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分类</h3>
+    <slot :games="games"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: ['title'],
+  data() {
+    return {
+      games: ['红色警戒', '穿越火线', '劲舞团', '超级玛丽'],
+    }
+  }
+}
+</script>
+
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+</style>
