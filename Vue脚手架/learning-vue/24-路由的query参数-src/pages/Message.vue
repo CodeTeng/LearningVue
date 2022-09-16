21df36b00f7c4f9ca05785cbc98e82365f5384bc
teng <teng2002823@outlook.com>
2022-09-16 23:47:31 +0800

    路由的query参数

---
+++ Vue脚手架/learning-vue/24-路由的query参数-src/pages/Message.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="m in messageList" :key="m.id">
+        <!--跳转并携带query参数，to的字符串写法        -->
+        <!--        <route-link :to="`/home/message/detail?id=${m.id}&title=${m.title}`">{{ m.title }}</route-link>-->
+        <!--跳转并携带query参数，to的对象写法（推荐）-->
+        <router-link
+            :to="{
+          name: 'detail', // 使用name进行跳转
+          query: {
+            id: m.id,
+            title: m.title,
+          }
+        }">
+          {{ m.title }}
+        </router-link>
+      </li>
+    </ul>
+    <hr/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  data() {
+    return {
+      messageList: [
+        {id: '001', title: '消息001'},
+        {id: '002', title: '消息002'},
+        {id: '003', title: '消息003'},
+      ]
+    }
+  }
+}
+</script>
---
+++ Vue脚手架/learning-vue/24-路由的query参数-src/router/index.js
@@ -0,0 +1,39 @@
+// 该文件专门用于创建整个应用的路由器
+import VueRouter from 'vue-router';
+// 引入组件
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+import News from "@/pages/News";
+import Message from "@/pages/Message";
+import Detail from "@/pages/Detail";
+
+// 创建并暴露一个路由器
+export default new VueRouter({
+    routes: [
+        {
+            path: '/home',
+            component: Home,
+            children: [
+                {
+                    path: "news",
+                    component: News,
+                },
+                {
+                    path: "message",
+                    component: Message,
+                    children: [
+                        {
+                            name: "detail", // name配置项为路由命名
+                            path: "detail",
+                            component: Detail,
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            path: '/about',
+            component: About,
+        }
+    ]
+})
