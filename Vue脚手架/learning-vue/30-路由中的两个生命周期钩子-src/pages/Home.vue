4dae78e2c66caaf7e72df3a0ab695c8b7ae0b733
teng <teng2002823@outlook.com>
2022-09-17 12:37:17 +0800

    路由中的两个生命周期钩子

---
+++ Vue脚手架/learning-vue/30-路由中的两个生命周期钩子-src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <ul class="nav nav-tabs">
+      <li>
+        <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+      </li>
+      <li>
+        <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+      </li>
+    </ul>
+    <keep-alive include="News">
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+}
+</script>
