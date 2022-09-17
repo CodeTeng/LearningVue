80a4b2725390bfabd371c61a203d0b916c2dc999
teng <teng2002823@outlook.com>
2022-09-17 12:36:43 +0800

    编程式路由导航

---
+++ Vue脚手架/learning-vue/28-编程式路由导航-src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <ul class="nav nav-tabs">
+      <li>
+        <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+      </li>
+      <li>
+        <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+}
+</script>
